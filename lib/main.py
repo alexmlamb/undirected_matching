a2bc59072ce4731d0708510880ef95d60282065a
Alexander Lamb <lambalex@leto07.iro.umontreal.ca>
2017-03-20 17:26:38 -0400

    updates

---
+++ lib/main.py
@@ -0,0 +1,25 @@
+'''
+-Initially make z_to_x and x_to_z fairly shallow networks.  Inject noise?  
+
+-Use the fflayer class?  
+
+'''
+import theano
+import theano.tensor as T
+
+def z_to_x(z):
+
+def x_to_z(x):
+
+def p_chain():
+
+def q_chain():
+
+
+def discriminator(x,z):
+    
+
+
+
+
+
